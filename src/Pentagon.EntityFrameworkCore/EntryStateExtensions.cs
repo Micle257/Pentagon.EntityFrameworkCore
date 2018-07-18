11074dfd55380e8b951e243de1b3451dc1a9517c
Michal Pokorný <michal.pokorny0@gmail.com>
2018-07-18 14:45:48 +0200

    added EntryStateExtensions.cs w/ mtd ToEntityStateType

---
+++ src/Pentagon.EntityFrameworkCore/EntryStateExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace Pentagon.EntityFrameworkCore {
+    using Microsoft.EntityFrameworkCore;
+
+    public static class EntryStateExtensions
+    {
+        public static EntityStateType ToEntityStateType(this EntityState state)
+        {
+            switch (state)
+            {
+                case EntityState.Deleted:
+                    return EntityStateType.Deleted;
+
+                case EntityState.Modified:
+                    return EntityStateType.Modified;
+
+                case EntityState.Added:
+                    return EntityStateType.Added;
+                    
+                default:
+                    return EntityStateType.Unspecified;
+            }
+        }
+    }
+}
