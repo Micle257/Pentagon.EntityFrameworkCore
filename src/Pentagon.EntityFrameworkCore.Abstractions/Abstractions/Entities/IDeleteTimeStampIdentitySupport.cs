7e3694166bafdfba46e516bbfb3e381aa8293b69
Michal Pokorný <michal.pokorny0@gmail.com>
2018-07-29 20:46:33 +0200

    TUserId now supports classes as well as structs

--- src/Pentagon.EntityFrameworkCore.Abstractions/Abstractions/Entities/IDeleteTimeStampIdentitySupport.cs
+++ src/Pentagon.EntityFrameworkCore.Abstractions/Abstractions/Entities/IDeleteTimeStampIdentitySupport.cs
@@ -7,7 +7,6 @@
 namespace Pentagon.EntityFrameworkCore.Abstractions.Entities
 {
     public interface IDeleteTimeStampIdentitySupport<TUserId> : IDeleteTimeStampIdentitySupport
-            where TUserId : struct
     {
         /// <summary> Gets or sets the user that deleted this entity. </summary>
         /// <value> The nullable <see cref="TUserId" />. </value>
