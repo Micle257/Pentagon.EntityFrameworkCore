02099d1aea89c5ad6868d528af22ebdfc72b7bf2
Michal Pokorný <michal.pokorny0@gmail.com>
2018-08-05 14:09:30 +0200

    added ParameterReplacer

---
+++ src/Pentagon.EntityFrameworkCore.Abstractions/ParameterReplacer.cs
@@ -0,0 +1,22 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="ParameterReplacer.cs">
+//   Copyright (c) Michal Pokorný. All Rights Reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+namespace Pentagon.EntityFrameworkCore
+{
+    using System.Linq.Expressions;
+
+    public class ParameterReplacer : ExpressionVisitor
+    {
+        readonly ParameterExpression _parameter;
+
+        public ParameterReplacer(ParameterExpression parameter)
+        {
+            _parameter = parameter;
+        }
+
+        protected override Expression VisitParameter(ParameterExpression node) => base.VisitParameter(_parameter);
+    }
+}
