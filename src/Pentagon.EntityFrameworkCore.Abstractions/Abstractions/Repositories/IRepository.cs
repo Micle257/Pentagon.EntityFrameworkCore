e29433199ca79082150dff47f4aee894dc753d04
Michal Pokorný <michal.pokorny0@gmail.com>
2019-02-15 11:58:55 +0100

    Repository - implements IQueryable

--- src/Pentagon.EntityFrameworkCore.Abstractions/Abstractions/Repositories/IRepository.cs
+++ src/Pentagon.EntityFrameworkCore.Abstractions/Abstractions/Repositories/IRepository.cs
@@ -18,17 +18,9 @@ namespace Pentagon.EntityFrameworkCore.Abstractions.Repositories
 
     /// <summary> Provides a standard interface for repository which is data-access mechanism agnostic with typed identifier. </summary>
     /// <typeparam name="TEntity"> The type of the entity. </typeparam>
-    public interface IRepository<TEntity> : IDeleteRepository<TEntity>, IInsertRepository<TEntity>, IUpdateRepository<TEntity>, IPagedRepository<TEntity>
+    public interface IRepository<TEntity> : IQueryable<TEntity>, IDeleteRepository<TEntity>, IInsertRepository<TEntity>, IUpdateRepository<TEntity>, IPagedRepository<TEntity>
             where TEntity : IEntity
     {
-        /// <summary> Gets the query. </summary>
-        /// <value> The query. </value>
-        IQueryable<TEntity> Query { get; }
-
-        /// <summary> Stop tracking the entity (detached from data source cache, eg. in EF Core detached from change tracker). </summary>
-        /// <param name="entity"> The entity. </param>
-        void Forget([NotNull] TEntity entity);
-
         /// <summary> Gets the selected entity. </summary>
         /// <param name="entitySelector"> The entity selector. </param>
         /// <param name="trackChanges"> If set to <c> true </c> the entity won't be track by change tracker. </param>
@@ -61,13 +53,7 @@ namespace Pentagon.EntityFrameworkCore.Abstractions.Repositories
 
         Task<IEnumerable<TEntity>> GetManyAsync<TSpecification>(TSpecification specification)
                 where TSpecification : IFilterSpecification<TEntity>, IOrderSpecification<TEntity>;
-
-        /// <summary> Gets a property, for navigational property. </summary>
-        /// <param name="foreignKey"> The foreign key to the navigational property. </param>
-        /// <returns> A <see cref="Task" /> that represents the asynchronous operation, result is the selected property. </returns>
-        Task<TProperty> GetPropertyByForeignKeyAsync<TProperty>(object foreignKey)
-                where TProperty : class;
-
+        
         /// <summary> Counts rows in repository. </summary>
         /// <returns> A <see cref="Task" /> that represents the asynchronous operation. Result of the operation is number of rows in repository. </returns>
         Task<int> CountAsync();
