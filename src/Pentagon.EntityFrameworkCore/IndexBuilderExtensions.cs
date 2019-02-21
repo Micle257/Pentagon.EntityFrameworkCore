0f1e8e171ea9ec3cc37230c17698ca03d74c83f7
Michal Pokorný <michal.pokorny0@gmail.com>
2019-02-21 18:10:18 +0100

    added extension method for deleted flag index filter

---
+++ src/Pentagon.EntityFrameworkCore/IndexBuilderExtensions.cs
@@ -0,0 +1,28 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="IndexBuilderExtensions.cs">
+//   Copyright (c) Michal Pokorný. All Rights Reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+namespace Pentagon.EntityFrameworkCore
+{
+    using System;
+    using Abstractions.Entities;
+    using JetBrains.Annotations;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    public static class IndexBuilderExtensions
+    {
+        [NotNull]
+        public static IndexBuilder<T> HasIndexForDeletedFlag<T>([NotNull] this IndexBuilder<T> builder)
+        {
+            if (builder == null)
+                throw new ArgumentNullException(nameof(builder));
+
+            builder.HasFilter($"{nameof(IDeletedFlagSupport.DeletedFlag)} = 0");
+
+            return builder;
+        }
+    }
+}
