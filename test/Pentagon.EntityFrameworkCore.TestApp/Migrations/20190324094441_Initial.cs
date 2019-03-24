e2df07f7b5b591f96ed36f945088595b57b4d56f
Michal Pokorný <michal.pokorny0@gmail.com>
2019-03-24 10:56:03 +0100

    added TestApp

---
+++ test/Pentagon.EntityFrameworkCore.TestApp/Migrations/20190324094441_Initial.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Pentagon.EntityFrameworkCore.TestApp.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Uuid = table.Column<Guid>(nullable: false, defaultValueSql: "NEWID()"),
+                    Name = table.Column<string>(maxLength: 256, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
---
+++ test/Pentagon.EntityFrameworkCore.TestApp/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Pentagon.EntityFrameworkCore.TestApp
+{
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Reflection;
+    using Abstractions;
+    using Abstractions.Entities;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+    using Microsoft.Extensions.DependencyInjection;
+    using Repositories;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var services = new ServiceCollection();
+
+            services.AddUnitOfWork<Context, IApplicationContext>();
+
+            var di = services.BuildServiceProvider();
+
+            using (var c = di.GetRequiredService<Context>())
+            {
+                
+            }
+        }
+    }
+
+    class User : Entity, ICreateStampSupport
+    {
+        /// <inheritdoc />
+        public Guid Uuid { get; set; }
+
+        public string Name { get; set; }
+    }
+
+    class UserConfig : IEntityTypeConfiguration<User>
+    {
+        /// <inheritdoc />
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.Property(a => a.Name)
+                   .IsRequired()
+                   .HasMaxLength(256);
+        }
+    }
+
+    class Context : BaseApplicationContext
+    {
+        /// <inheritdoc />
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer("Data Source=PENTAGONDELL\\SQLEXPRESS;Initial Catalog=PentagonEFTest;Integrated Security=True");
+        }
+
+        /// <inheritdoc />
+        protected override void OnModelCreatingCore(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
