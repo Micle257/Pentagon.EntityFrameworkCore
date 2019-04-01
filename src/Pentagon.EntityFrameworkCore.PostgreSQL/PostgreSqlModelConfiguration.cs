1473f65f2ce88a784ecfbc93a703e53c202095be
Michal Pokorný <michal.pokorny0@gmail.com>
2019-04-01 11:53:51 +0200

    added PostgreSQL project

---
+++ src/Pentagon.EntityFrameworkCore.PostgreSQL/PostgreSqlModelConfiguration.cs
@@ -0,0 +1,22 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="PostgreSqlModelConfiguration.cs">
+//   Copyright (c) Michal Pokorný. All Rights Reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+namespace Pentagon.EntityFrameworkCore.PostgreSQL
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class PostgreSqlModelConfiguration : ModelConfiguration
+    {
+        /// <inheritdoc />
+        protected override string DefaultCreateGuidDatabaseFunction => "uuid_generate_v4()";
+
+        /// <inheritdoc />
+        protected override void SetupModelCore(ModelBuilder builder)
+        {
+            builder.HasPostgresExtension(name: "uuid-ossp");
+        }
+    }
+}
