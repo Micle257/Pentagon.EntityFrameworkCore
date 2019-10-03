3191fe9e5cf98dfeaf433b8ee5df6d67e27912be
Michal Pokorný <michal.pokorny0@gmail.com>
2019-10-03 10:45:23 +0200

    removed old code

--- src/Pentagon.EntityFrameworkCore/Extensions/QueryableExtensions.cs
+++ src/Pentagon.EntityFrameworkCore/Extensions/QueryableExtensions.cs
@@ -21,28 +21,6 @@ namespace Pentagon.EntityFrameworkCore.Extensions
 
     public static class QueryableExtensions
     {
-        static readonly TypeInfo QueryCompilerTypeInfo = typeof(QueryCompiler).GetTypeInfo();
-
-        static readonly FieldInfo QueryCompilerField = typeof(EntityQueryProvider).GetTypeInfo().DeclaredFields.First(x => x.Name == "_queryCompiler");
-        static readonly FieldInfo QueryModelGeneratorField = typeof(QueryCompiler).GetTypeInfo().DeclaredFields.First(x => x.Name == "_queryModelGenerator");
-        static readonly FieldInfo DataBaseField = QueryCompilerTypeInfo.DeclaredFields.Single(x => x.Name == "_database");
-        static readonly PropertyInfo DatabaseDependenciesField = typeof(Database).GetTypeInfo().DeclaredProperties.Single(x => x.Name == "Dependencies");
-
-        public static string ToSql<TEntity>(this IQueryable<TEntity> query)
-        {
-            var queryCompiler = (QueryCompiler) QueryCompilerField.GetValue(query.Provider);
-            var queryModelGenerator = (QueryModelGenerator) QueryModelGeneratorField.GetValue(queryCompiler);
-            var queryModel = queryModelGenerator.ParseQuery(query.Expression);
-            var database = DataBaseField.GetValue(queryCompiler);
-            var databaseDependencies = (DatabaseDependencies) DatabaseDependenciesField.GetValue(database);
-            var queryCompilationContext = databaseDependencies.QueryCompilationContextFactory.Create(false);
-            var modelVisitor = (RelationalQueryModelVisitor) queryCompilationContext.CreateQueryModelVisitor();
-            modelVisitor.CreateQueryExecutor<TEntity>(queryModel);
-            var sql = modelVisitor.Queries.First().ToString();
-
-            return sql;
-        }
-
         public static Task<PagedList<TEntity>> ToPagedListAsync<TEntity>([NotNull] this IQueryable<TEntity> query, int pageNumber, int pageSize)
         {
             return ToPagedListAsync(query, new PaginationParameters {PageSize = pageSize, PageNumber = pageNumber});
